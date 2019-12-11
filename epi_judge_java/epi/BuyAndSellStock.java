d82235957ce8bf6e7246788e72811b8d06757d25
junrong09 <junrongy09@gmail.com>
2019-12-11 12:41:37 +0800

    complete with O(n)

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -6,7 +6,16 @@ public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
     // TODO - you fill in here.
-    return 0.0;
+    double max = 0, stock = Double.MAX_VALUE;
+    int p = 0;
+    while (p < prices.size()) {
+	// Always attempt to calculate profit/loss and record max
+	max = Math.max(max, prices.get(p) - stock);
+	// Try to get the cheapest stock
+	stock = Math.min(stock, prices.get(p));
+	++p;
+    }
+    return max;
   }
 
   public static void main(String[] args) {
