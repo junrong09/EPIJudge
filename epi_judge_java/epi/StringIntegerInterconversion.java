3716d9e0f5108383175975031ad4a3dbd4ccd1f3
junrong09 <junrongy09@gmail.com>
2019-12-11 13:18:14 +0800

    Complete with improvement

--- epi_judge_java/epi/StringIntegerInterconversion.java
+++ epi_judge_java/epi/StringIntegerInterconversion.java
@@ -2,46 +2,34 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
-import java.util.ArrayList;
-import java.util.List;
 public class StringIntegerInterconversion {
 
   public static String intToString(int x) {
     // TODO - you fill in here.
     StringBuilder sb = new StringBuilder();
-    List<Character> list = new ArrayList<>();
     boolean neg = x < 0;
-    x = Math.abs(x);
     while (x != 0) {
-	    int v = x % 10;
+	    int v = Math.abs(x % 10);
 	    x /= 10;
-	    list.add((char) (v + '0'));
+	    sb.append(v);
     }
-    if (neg) list.add('-');
-    for (int i = list.size() - 1; i >= 0; i--) {
-	    sb.append(list.get(i));
-    }
-    String s = sb.toString();
-    return s.isEmpty() ? "0" : s;
+    if (neg) sb.append('-');
+    return sb.length() == 0 ? "0" : sb.reverse().toString();
+    
   }
 
   public static int stringToInt(String s) {
     // TODO - you fill in here.
-    int result = 0, digit = 0;
     char[] cc = s.toCharArray();
-    while (digit < cc.length) {
-	    switch (cc[cc.length - digit - 1]) {
-		    case '-':
-			    result *= -1;
-			    break;
-		    default:
-			    int val = cc[cc.length - digit - 1] - '0';
-			    result += val * Math.pow(10, digit);
-			    
+    long result = 0;
+
+    for (char c : cc) {
+	    if (c != '-') {
+		    result = (result * 10) + (c - '0');
 	    }
-	    digit++;
     }
-    return result;
+    if (cc[0] == '-') result *= -1;
+    return (int) result;
   }
 
   @EpiTest(testDataFile = "string_integer_interconversion.tsv")
