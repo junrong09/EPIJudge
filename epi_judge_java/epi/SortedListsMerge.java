f45b55ddd6083d616abb1383210f37a90cdc9850
junrong09 <junrongy09@gmail.com>
2019-12-11 15:36:41 +0800

    Complete in O(n+m)

--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -7,7 +7,26 @@ public class SortedListsMerge {
   public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
                                                       ListNode<Integer> L2) {
     // TODO - you fill in here.
-    return null;
+    ListNode<Integer> head = new ListNode<>(0, null), p = head;
+    while (L1 != null && L2 != null) {
+	    if (L1.data < L2.data) {
+		    p.next = L1;
+		    L1 = L1.next;
+	    } else {
+		    p.next = L2;
+		    L2 = L2.next;
+	    }
+	    p = p.next;
+    }
+
+    if (L1 != null) {
+	    p.next = L1;
+    }
+
+    if (L2 != null) {
+	    p.next = L2;
+    }
+    return head.next;
   }
 
   public static void main(String[] args) {
