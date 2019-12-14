7e791bed182b21ea6544d6b3b0dcdab892a85ef7
junrong09 <junrongy09@gmail.com>
2019-12-14 19:51:59 +0800

    Complete with 2 solutions O(n + m) and O(n log m). But sol 1 failed last test case

--- epi_judge_java/epi/IntersectSortedArrays.java
+++ epi_judge_java/epi/IntersectSortedArrays.java
@@ -2,13 +2,56 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 public class IntersectSortedArrays {
   @EpiTest(testDataFile = "intersect_sorted_arrays.tsv")
 
   public static List<Integer> intersectTwoSortedArrays(List<Integer> A,
                                                        List<Integer> B) {
     // TODO - you fill in here.
-    return null;
+    //return implementation1(A,B);
+    return implementation2(A,B);
+  }
+
+  // O(n + m)
+  private static List<Integer> implementation1(List<Integer> A, List<Integer> B) {
+    List<Integer> result = new ArrayList<>();
+    int a = 0, b = 0, found = 0;
+    while (a < A.size() && b < B.size()) {
+      if (A.get(a) == B.get(b)) {
+        if (A.get(a) != found || result.isEmpty()) {
+          found = A.get(a);
+          result.add(A.get(a));
+        }
+        a++;
+        b++;
+      } else if (A.get(a) > B.get(b)) {
+        b++;
+      } else {
+        a++;
+      }
+    }
+    return result;
+  }
+
+  // O( n log m), m > n
+  private static List<Integer> implementation2(List<Integer> A, List<Integer> B) {
+    List<Integer> result = new ArrayList<>();
+    if (A.size() > B.size()) {
+      List<Integer> temp = B;
+      B = A;
+      A = temp;
+    }
+
+    int prev = A.isEmpty() ? 0 : A.get(0) - 1;
+    for (int v : A) {
+      if (v != prev && Collections.binarySearch(B, v) >= 0) {
+        result.add(v);
+      }
+      prev = v;
+    }
+    return result;
   }
 
   public static void main(String[] args) {
