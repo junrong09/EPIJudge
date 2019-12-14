27c49e6a45632b27ee303143dd03d258bc007532
junrong09 <junrongy09@gmail.com>
2019-12-14 15:53:25 +0800

    Complete in O(n), O(n). But don't have to check if length of string is odd. Just only allow 1 odd regardless of length of string. Because for an even length string, if there exist a odd, there must be another odd and since odd > 1, it is not a palindrome.

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -1,13 +1,32 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.util.Map;
+import java.util.HashMap;
+
 public class IsStringPermutableToPalindrome {
   @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
   public static boolean canFormPalindrome(String s) {
     // TODO - you fill in here.
+    char[] cc = s.toCharArray();
+    boolean allowOdd = cc.length % 2 == 1;
+    Map<Character, Integer> map = new HashMap<>();
+    for (char c : cc) {
+      map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+    for (Map.Entry<Character, Integer> entry: map.entrySet()) {
+      if (entry.getValue() % 2 == 1) {
+        if (allowOdd) {
+          allowOdd = false;
+        } else {
+          return false;
+        }
+      }
+    }
     return true;
   }
+
 
   public static void main(String[] args) {
     System.exit(
