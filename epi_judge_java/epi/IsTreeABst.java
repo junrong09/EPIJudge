ed77f9695018ef5b43b2875319f515d2e8b909d6
junrong09 <junrongy09@gmail.com>
2019-12-15 22:30:33 +0800

    Complete in O(n), O(h). Non-recursive implementation with variant that left tree should be between MIN and val of current node (right tree should be between val of current and MAX)

--- epi_judge_java/epi/IsTreeABst.java
+++ epi_judge_java/epi/IsTreeABst.java
@@ -3,35 +3,41 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import java.util.Arrays;
 import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 public class IsTreeABst {
-  @EpiTest(testDataFile = "is_tree_a_bst.tsv")
 
+  public static class Entry {
+      BinaryTreeNode<Integer> tree;
+      int min = Integer.MIN_VALUE;
+      int max = Integer.MAX_VALUE;
+
+      public Entry(BinaryTreeNode<Integer> tree, int min, int max) {
+        this.tree = tree;
+        this.min = min;
+        this.max = max;
+      }
+    }
+
+  @EpiTest(testDataFile = "is_tree_a_bst.tsv")
   public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> tree) {
     // TODO - you fill in here.
-    return isBST(tree, Arrays.asList(0,0));
-  }
-
-  private static boolean isBST(BinaryTreeNode<Integer> tree, List<Integer> stats) {
-    if (tree == null) {
-      stats.set(0, null);
-      stats.set(1, null);
-      return true;
+    Queue<Entry> q = new LinkedList<>();
+    q.add(new Entry(tree, Integer.MIN_VALUE, Integer.MAX_VALUE));
+    
+    while (!q.isEmpty()) {
+      Entry e = q.poll();
+      if (e.tree != null) {
+        if (e.tree.data >= e.min && e.tree.data <= e.max) {
+          q.add(new Entry(e.tree.left, e.min, e.tree.data));
+          q.add(new Entry(e.tree.right, e.tree.data, e.max));
+        } else {
+          return false;
+        }
+      }
     }
-
-    List<Integer> s1 = Arrays.asList(0, 0), s2 = Arrays.asList(0,0);
-    if (!isBST(tree.left, s1) || !isBST(tree.right, s2)) {
-      return false;
-    }
-
-    if ((s1.get(0) != null && tree.data < s1.get(1)) || (s2.get(0) != null && tree.data > s2.get(0))) {
-      return false;
-    }
-
-    stats.set(0, s1.get(0) == null ? tree.data : s1.get(0));
-    stats.set(1, s2.get(1) == null ? tree.data : s2.get(1));
     return true;
   }
-
 
   public static void main(String[] args) {
     System.exit(
