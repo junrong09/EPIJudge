1594892bbfa265af66f7e1c467ba6c787fb573df
junrong09 <junrongy09@gmail.com>
2019-12-24 14:27:59 +0800

    Complete in O(log k) time, O(1) space

--- epi_judge_java/epi/IntSquareRoot.java
+++ epi_judge_java/epi/IntSquareRoot.java
@@ -6,7 +6,18 @@ public class IntSquareRoot {
 
   public static int squareRoot(int k) {
     // TODO - you fill in here.
-    return 0;
+    int s = 0, e = k;
+    while (s != e && s+1 != e) {
+      int m = s + (e-s)/2;
+      if ((long) m*m == k) {
+        return m;
+      } else if ((long) m*m < k) {
+        s = m;
+      } else {
+        e = m - 1;
+      }
+    }
+    return e * e > k ? s : e;
   }
 
   public static void main(String[] args) {
