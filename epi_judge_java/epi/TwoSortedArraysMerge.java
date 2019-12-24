22360ed5b108f6a2be0b1d3a217311ecee96af2d
junrong09 <junrongy09@gmail.com>
2019-12-24 21:33:52 +0800

    Complete in O(2m + n) time, O(1) space

--- epi_judge_java/epi/TwoSortedArraysMerge.java
+++ epi_judge_java/epi/TwoSortedArraysMerge.java
@@ -2,12 +2,37 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import java.util.List;
+import java.util.Collections;
+
 public class TwoSortedArraysMerge {
 
   public static void mergeTwoSortedArrays(List<Integer> A, int m,
                                           List<Integer> B, int n) {
     // TODO - you fill in here.
-    return;
+    int p = m + n;
+    for (int i = m - 1; i >= 0; i--) {
+      Collections.swap(A, i, --p);
+    }
+
+    int p2 = 0, i = 0;
+    while (p2 < n && p < m + n) {
+      if (A.get(p) < B.get(p2)) {
+        A.set(i, A.get(p));
+        p++;
+      } else {
+        A.set(i, B.get(p2));
+        p2++;
+      }
+      i++;
+    }
+
+    while (p2 < n) {
+      A.set(i++, B.get(p2++));
+    }
+
+    while (p < m + n) {
+      A.set(i++, A.get(p++));
+    }
   }
   @EpiTest(testDataFile = "two_sorted_arrays_merge.tsv")
   public static List<Integer>
