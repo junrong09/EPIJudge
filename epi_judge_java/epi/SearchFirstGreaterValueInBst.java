dab390b533293e30514da547f5153aa5f193b8be
junrong09 <junrongy09@gmail.com>
2019-12-29 17:00:25 +0800

    Complete in O(h) time, O(1) space

--- epi_judge_java/epi/SearchFirstGreaterValueInBst.java
+++ epi_judge_java/epi/SearchFirstGreaterValueInBst.java
@@ -1,12 +1,23 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SearchFirstGreaterValueInBst {
 
   public static BstNode<Integer> findFirstGreaterThanK(BstNode<Integer> tree,
                                                        Integer k) {
     // TODO - you fill in here.
-    return null;
+    BstNode<Integer> max = new BstNode(Integer.MAX_VALUE);
+    BstNode<Integer> poss = max;
+    while (tree != null) {
+      if (tree.data > k) {
+        if (tree.data <= poss.data) poss = tree; // No need to check because of the way the tree is being traversed, this is always true.
+        tree = tree.left;
+      } else {
+        tree = tree.right;
+      }
+    }
+    return poss == max ? new BstNode(-1) : poss;
   }
   @EpiTest(testDataFile = "search_first_greater_value_in_bst.tsv")
   public static int findFirstGreaterThanKWrapper(BstNode<Integer> tree,
