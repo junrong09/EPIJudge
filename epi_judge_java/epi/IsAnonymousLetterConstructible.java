8df70ce2add3753ba4af11928a1745de4986495d
junrong09 <junrongy09@gmail.com>
2020-01-06 21:49:34 +0800

    Complete in O(n + m) time, O(c) space where c is number of distinct characters in the letter.

--- epi_judge_java/epi/IsAnonymousLetterConstructible.java
+++ epi_judge_java/epi/IsAnonymousLetterConstructible.java
@@ -1,12 +1,28 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.util.Map;
+import java.util.HashMap;
+
 public class IsAnonymousLetterConstructible {
   @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
 
   public static boolean isLetterConstructibleFromMagazine(String letterText,
                                                           String magazineText) {
     // TODO - you fill in here.
+    Map<Character, Integer> map = new HashMap<>();
+    for (char c : letterText.toCharArray()) {
+      map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+
+    for (char c : magazineText.toCharArray()) {
+      if (map.containsKey(c))
+        map.put(c, map.get(c) - 1);
+    }
+
+    for (int i : map.values()) {
+      if (i > 0) return false;
+    }
     return true;
   }
 
