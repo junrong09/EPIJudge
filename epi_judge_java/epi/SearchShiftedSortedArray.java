66575966870ef6d05d9d2bf5f2282c6ae92b8203
junrong09 <junrongy09@gmail.com>
2020-01-06 20:09:07 +0800

    Complete in O(log n) time, O(log n) space [because of recursion]. Can be improved with iteration and not tracking of min.

--- epi_judge_java/epi/SearchShiftedSortedArray.java
+++ epi_judge_java/epi/SearchShiftedSortedArray.java
@@ -7,7 +7,18 @@ public class SearchShiftedSortedArray {
 
   public static int searchSmallest(List<Integer> A) {
     // TODO - you fill in here.
-    return 0;
+    return search(A, 0, A.size() - 1, A.get(0), 0);
+  }
+
+  private static int search(List<Integer> A, int s, int e, int p, int min) {
+    if (s > e) return min;
+    int mid = ((e - s) / 2) + s;
+    min = (A.get(mid) < A.get(min)) ? mid : min;
+    if (A.get(mid) >= p) {
+      return search(A, mid + 1, e, p, min);
+    } else {
+      return search(A, s, mid - 1, p, min);
+    }
   }
 
   public static void main(String[] args) {
