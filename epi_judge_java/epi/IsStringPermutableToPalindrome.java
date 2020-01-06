6a0c35aab749894d0374553e1f2b3e7037f380fe
junrong09 <junrongy09@gmail.com>
2020-01-06 21:29:24 +0800

    Complete in O(n) time, O(c) space where c is the number of distinct character. This is a simple implementation using set to detect pairs instead of counting the frequency.

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -1,30 +1,22 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
 
 public class IsStringPermutableToPalindrome {
   @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
   public static boolean canFormPalindrome(String s) {
     // TODO - you fill in here.
+    Set<Character> set = new HashSet<>();
     char[] cc = s.toCharArray();
-    boolean allowOdd = cc.length % 2 == 1;
-    Map<Character, Integer> map = new HashMap<>();
     for (char c : cc) {
-      map.put(c, map.getOrDefault(c, 0) + 1);
-    }
-    for (Map.Entry<Character, Integer> entry: map.entrySet()) {
-      if (entry.getValue() % 2 == 1) {
-        if (allowOdd) {
-          allowOdd = false;
-        } else {
-          return false;
-        }
+      if (!set.remove(c)) {
+        set.add(c);
       }
     }
-    return true;
+    return (set.size() <= 1);
   }
 
 
