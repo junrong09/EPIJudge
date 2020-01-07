46cd8762d7d3ae4b8ce0ed26072ef7207546063c
junrong09 <junrongy09@gmail.com>
2020-01-07 16:02:01 +0800

    Complete in O(n log n) time, O(n) space. Take note of event with the same timing. Start timing must be processed first before end timing to keep the concurrent accurate.

--- epi_judge_java/epi/CalendarRendering.java
+++ epi_judge_java/epi/CalendarRendering.java
@@ -3,6 +3,10 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
 public class CalendarRendering {
   @EpiUserType(ctorParams = {int.class, int.class})
 
@@ -29,7 +33,35 @@ public class CalendarRendering {
 
   public static int findMaxSimultaneousEvents(List<Event> A) {
     // TODO - you fill in here.
-    return 0;
+    PriorityQueue<List<Integer>> pq = new PriorityQueue<>((x,y) -> x.get(1) - y.get(1));
+    for (int i = 0; i < A.size(); i++) {
+      pq.add(Arrays.asList(i, A.get(i).start));
+      pq.add(Arrays.asList(i, A.get(i).finish));
+    }
+    Set<Integer> current = new HashSet<>();
+    int max = 0;
+    while (!pq.isEmpty()) {
+      List<Integer> event = pq.poll();
+      Set<Integer> remove = new HashSet<>();
+      while (!pq.isEmpty() && pq.peek().get(1) == event.get(1)) {
+        int eventId = pq.poll().get(0);
+        if (current.contains(eventId)) {
+          remove.add(eventId);
+        } else {
+          current.add(eventId);
+        }
+      }
+
+      if (current.contains(event.get(0))) {
+        remove.add(event.get(0));
+      } else {
+        current.add(event.get(0));
+      }
+
+      max = Math.max(max, current.size());
+      current.removeAll(remove);
+    }
+    return max;
   }
 
   public static void main(String[] args) {
