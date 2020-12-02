71ce6ccb83bd7b1743ed8f1b80f4863dbe372206
junrong09 <junrongy09@gmail.com>
2020-12-03 00:44:38 +0800

    Add completed majority element

--- epi_judge_java/epi/MajorityElement.java
+++ epi_judge_java/epi/MajorityElement.java
@@ -8,8 +8,24 @@ public class MajorityElement {
 
   public static String majoritySearch(Iterator<String> stream) {
     // TODO - you fill in here.
-    return "";
+    // O(n); O(1)
+    String s = "";
+    int count = 0;
+    while (stream.hasNext()) {
+      String temp = stream.next();
+      if (temp.equals(s)) {
+        count++;
+      } else {
+        count--;
+        if (count < 1) {
+          s = temp;
+          count = 1;
+        }
+      }
+    }
+    return s;
   }
+  
   @EpiTest(testDataFile = "majority_element.tsv")
   public static String majoritySearchWrapper(List<String> stream) {
     return majoritySearch(stream.iterator());
