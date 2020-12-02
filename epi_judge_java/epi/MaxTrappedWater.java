c2f5171e1ea931f5b6647472d496ffd8f2ff3e81
junrong09 <junrongy09@gmail.com>
2020-12-03 00:50:14 +0800

    Add completed max trapped water

--- epi_judge_java/epi/MaxTrappedWater.java
+++ epi_judge_java/epi/MaxTrappedWater.java
@@ -8,7 +8,21 @@ public class MaxTrappedWater {
 
   public static int getMaxTrappedWater(List<Integer> heights) {
     // TODO - you fill in here.
-    return 0;
+    // O(n); O(1)
+    int max = 0, i = 0, j = heights.size() - 1;
+    while (i < j) {
+      int area = (j - i) * Math.min(heights.get(i), heights.get(j));
+      max = Math.max(area, max);
+      if (heights.get(i) > heights.get(j)) {
+        j--;
+      } else if (heights.get(i) < heights.get(j)) {
+        i++;
+      } else {
+        i++;
+        j--;
+      }
+    }
+    return max;
   }
 
   public static void main(String[] args) {
