b5ced30b8626e59d8cb84354ccc852651562bbd6
junrong09 <junrongy09@gmail.com>
2020-12-02 15:56:46 +0800

    Add completed LevenshiteinDistance

--- epi_judge_java/epi/LevenshteinDistance.java
+++ epi_judge_java/epi/LevenshteinDistance.java
@@ -1,12 +1,33 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.util.*;
 public class LevenshteinDistance {
   @EpiTest(testDataFile = "levenshtein_distance.tsv")
 
   public static int levenshteinDistance(String A, String B) {
     // TODO - you fill in here.
-    return 0;
+    // O(n^2), O(n^2)
+    Map<List<Integer>, Integer> map = new HashMap<>();
+    return distance(A, A.length() - 1, B, B.length() - 1, map);
+  }
+
+  public static int distance(String A, int a, String B, int b, Map<List<Integer>, Integer> map) {
+    if (map.containsKey(Arrays.asList(a, b))) {
+      return map.get(Arrays.asList(a, b));
+    }
+    int val = 0;
+    if (b < 0) {
+      val = a + 1;
+    } else if (a < 0) {
+      val = b + 1;
+    } else if (A.charAt(a) == B.charAt(b)) {
+      val = distance(A, a - 1, B, b - 1, map);
+    } else {
+      val = 1 + Math.min(Math.min(distance(A, a - 1, B, b - 1, map), distance(A, a, B, b - 1, map)), distance(A, a - 1, B, b, map));
+    }
+    map.put(Arrays.asList(a, b), val);
+    return val;
   }
 
   public static void main(String[] args) {
