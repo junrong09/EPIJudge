f898929c79e3cedc508249217fc09e07d1f688a5
junrong09 <junrongy09@gmail.com>
2020-12-02 16:41:07 +0800

    Add completed ways to traverse 2D array

--- epi_judge_java/epi/NumberOfTraversalsMatrix.java
+++ epi_judge_java/epi/NumberOfTraversalsMatrix.java
@@ -6,14 +6,28 @@ public class NumberOfTraversalsMatrix {
 
   public static int numberOfWays(int n, int m) {
     // TODO - you fill in here.
-    return 0;
+    // O(n^2); O(n^2)
+    if (n == 1 && m == 1) return 1;
+    int[][] counts = new int[n][m];
+    for (int j = 1; j < m; j++) {
+      counts[0][j] = 1;
+    }
+    for (int i = 1; i < n; i++) {
+      counts[i][0] = 1;
+    }
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < m; j++) {
+        counts[i][j] = counts[i-1][j] + counts[i][j-1];
+      }
+    }
+    return counts[n-1][m-1];
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
-            .runFromAnnotations(args, "NumberOfTraversalsMatrix.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+        .runFromAnnotations(args, "NumberOfTraversalsMatrix.java",
+          new Object() {}.getClass().getEnclosingClass())
+        .ordinal());
   }
 }
