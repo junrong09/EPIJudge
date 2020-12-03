29f703ef307f839fa873863c1a5fd97aa88c503a
junrong09 <junrongy09@gmail.com>
2020-12-03 18:49:49 +0800

    Add completed Matrix connected regions

--- epi_judge_java/epi/MatrixConnectedRegions.java
+++ epi_judge_java/epi/MatrixConnectedRegions.java
@@ -4,12 +4,45 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 public class MatrixConnectedRegions {
   public static void flipColor(int x, int y, List<List<Boolean>> image) {
     // TODO - you fill in here.
-    return;
+    // O(V + E); O(V + E)
+    flip(x, y, !image.get(x).get(y), image);
+    //bfs(x, y, !image.get(x).get(y), image);
   }
+
+  public static void bfs(int x, int y, boolean flipTo, List<List<Boolean>> image) {
+    Queue<List<Integer>> queue = new LinkedList<>();
+    queue.add(Arrays.asList(x, y));
+    int[][] changes = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
+    while (queue.peek() != null) {
+      List<Integer> item = queue.poll();
+      if (image.get(item.get(0)).get(item.get(1)) == flipTo) {
+        continue;
+      }
+      image.get(item.get(0)).set(item.get(1), flipTo);
+      for (int[] c : changes) {
+        int newX = item.get(0) + c[0], newY = item.get(1) + c[1];
+        if (newX >= 0 && newY >= 0 && newX < image.size() && newY < image.get(0).size()) {
+          queue.add(Arrays.asList(newX, newY));
+        }
+      }
+    }
+  }
+
+  public static void flip(int x, int y, boolean flipTo, List<List<Boolean>> image) {
+    if (x < 0 || y < 0 || x >= image.size() || y >= image.get(0).size() || flipTo == image.get(x).get(y)) {
+      return;
+    }
+    image.get(x).set(y, flipTo);
+    int[][] changes = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
+    for (int[] c : changes) {
+      flip(x + c[0], y + c[1], flipTo, image);
+    }
+  }
+
   @EpiTest(testDataFile = "painting.tsv")
   public static List<List<Integer>> flipColorWrapper(TimedExecutor executor,
                                                      int x, int y,
