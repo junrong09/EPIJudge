743c57c7ce90ef53d3676f610f7f3c44886a892c
junrong09 <junrongy09@gmail.com>
2020-12-04 13:58:53 +0800

    Add completed is tree balanced

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -7,7 +7,20 @@ public class IsTreeBalanced {
 
   public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
     // TODO - you fill in here.
-    return true;
+    return cal(tree, new int[1]);
+  }
+
+  public static boolean cal(BinaryTreeNode<Integer> tree, int[] A) {
+    if (tree == null) {
+      A[0] = 0;
+      return true;
+    }
+    int[] A1 = new int[1];
+    int[] A2 = new int[1];
+    boolean result = cal(tree.left, A1) && cal(tree.right, A2);
+    A[0] = Math.max(A1[0], A2[0]) + 1;
+    if (Math.abs(A1[0] - A2[0]) > 1) result = false;
+    return result;
   }
 
   public static void main(String[] args) {
