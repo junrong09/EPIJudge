973e908e7a84109cdd0a8d55e61b9d16ee75fa61
junrong09 <junrongy09@gmail.com>
2020-12-07 13:04:52 +0800

    Add completed is tree sym

--- epi_judge_java/epi/IsTreeSymmetric.java
+++ epi_judge_java/epi/IsTreeSymmetric.java
@@ -1,11 +1,48 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.util.*;
 public class IsTreeSymmetric {
   @EpiTest(testDataFile = "is_tree_symmetric.tsv")
 
   public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
     // TODO - you fill in here.
+    List<BinaryTreeNode<Integer>> toVisit = new ArrayList<>();
+    List<BinaryTreeNode<Integer>> next = new ArrayList<>();
+    toVisit.add(tree);
+    while (!toVisit.isEmpty()) {
+      if (!isPalin(toVisit)) {
+        return false;
+      }
+      for (BinaryTreeNode<Integer> n : toVisit) {
+        if (n == null) {
+          continue;
+        }
+        next.add(n.left);
+        next.add(n.right);
+      }
+      toVisit = next;
+      next = new ArrayList<>();
+    }
+    return true;
+  }
+
+  public static boolean isPalin(List<BinaryTreeNode<Integer>> list) {
+    int i = 0, j = list.size() - 1;
+    while (i < j) {
+      BinaryTreeNode<Integer> r = list.get(i), l = list.get(j);
+      if (r == null || l == null) {
+        if (r != l) {
+          return false;
+        }
+      } else {
+        if (r.data != l.data) {
+          return false;
+        }
+      }
+      i++;
+      j--;
+    }
     return true;
   }
 
