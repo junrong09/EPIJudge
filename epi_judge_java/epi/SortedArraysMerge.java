f8112b6db30c2ca552c8b6b815c5c4cb8951bf4e
junrong09 <junrongy09@gmail.com>
2020-12-07 18:18:56 +0800

    Add completed sortedarraysmerge

--- epi_judge_java/epi/SortedArraysMerge.java
+++ epi_judge_java/epi/SortedArraysMerge.java
@@ -2,14 +2,29 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
+import java.util.*;
 public class SortedArraysMerge {
   @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
 
   public static List<Integer>
   mergeSortedArrays(List<List<Integer>> sortedArrays) {
     // TODO - you fill in here.
-    return null;
+    // O(n lg k); O(k)
+    Queue<List<Integer>> queue = new PriorityQueue<>((x,y) -> x.get(0)-y.get(0));
+    List<Integer> result = new ArrayList<>();
+    int[] indexes = new int[sortedArrays.size()];
+    for (int i = 0; i < indexes.length; i++) {
+      queue.add(Arrays.asList(sortedArrays.get(i).get(indexes[i]++), i));
+    }
+    while (!queue.isEmpty()) {
+      List<Integer> r = queue.poll();
+      result.add(r.get(0));
+      int listNum = r.get(1);
+      if (indexes[listNum] < sortedArrays.get(listNum).size()) {
+        queue.add(Arrays.asList(sortedArrays.get(listNum).get(indexes[listNum]++), listNum));
+      }
+    }
+    return result;
   }
 
   public static void main(String[] args) {
