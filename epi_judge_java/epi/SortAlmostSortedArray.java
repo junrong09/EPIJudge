04e9cd9bed1a3ba847b91b6a067e4d2c5ef2b942
junrong09 <junrongy09@gmail.com>
2020-12-07 20:27:00 +0800

    Add completed sort almost sorted array

--- epi_judge_java/epi/SortAlmostSortedArray.java
+++ epi_judge_java/epi/SortAlmostSortedArray.java
@@ -2,15 +2,28 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.Iterator;
+import java.util.*;
 import java.util.List;
 public class SortAlmostSortedArray {
 
   public static List<Integer>
   sortApproximatelySortedData(Iterator<Integer> sequence, int k) {
     // TODO - you fill in here.
-    return null;
+    // O(n lg k); O(k)
+    Queue<Integer> q = new PriorityQueue<>();
+    List<Integer> result = new ArrayList<>();
+    for (int i = 0; i <= k; i++) {
+      if (sequence.hasNext()) {
+        q.add(sequence.next());
+      }
+    }
+    while (!q.isEmpty()) {
+      result.add(q.poll());
+      if (sequence.hasNext()) q.add(sequence.next());
+    }
+    return result;
   }
+
   @EpiTest(testDataFile = "sort_almost_sorted_array.tsv")
   public static List<Integer>
   sortApproximatelySortedDataWrapper(List<Integer> sequence, int k) {
