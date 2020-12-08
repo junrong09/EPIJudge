aa93c08f804cea589587301d200298c7ce552cd3
junrong09 <junrongy09@gmail.com>
2020-12-08 22:23:51 +0800

    Add completed anagrams

--- epi_judge_java/epi/Anagrams.java
+++ epi_judge_java/epi/Anagrams.java
@@ -4,7 +4,7 @@ import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 import epi.test_framework.LexicographicalListComparator;
 
-import java.util.Collections;
+import java.util.*;
 import java.util.List;
 import java.util.function.BiPredicate;
 public class Anagrams {
@@ -12,11 +12,33 @@ public class Anagrams {
 
   public static List<List<String>> findAnagrams(List<String> dictionary) {
     // TODO - you fill in here.
-    return null;
+    // O(n * m lg m); O(n)
+    Map<String, List<String>> result = new HashMap<>();
+    for (String s : dictionary) {
+      char[] chars = s.toCharArray();
+      Arrays.sort(chars);
+      String sortedString = new String(chars);
+      if (result.containsKey(sortedString)) {
+        List<String> l = result.get(sortedString);
+        l.add(s);
+      } else {
+        List<String> l = new ArrayList<>();
+        l.add(s);
+        result.put(sortedString, l);
+      }
+    }
+    List<List<String>> A = new ArrayList<List<String>>();
+    for (List<String> r : result.values()) {
+      if (r.size() > 1) {
+        A.add(r);
+      }
+    }
+    return A;
   }
+
   @EpiTestComparator
   public static boolean comp(List<List<String>> expected,
-                             List<List<String>> result) {
+      List<List<String>> result) {
     if (result == null) {
       return false;
     }
@@ -34,8 +56,8 @@ public class Anagrams {
   public static void main(String[] args) {
     System.exit(
         GenericTest
-            .runFromAnnotations(args, "Anagrams.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+        .runFromAnnotations(args, "Anagrams.java",
+          new Object() {}.getClass().getEnclosingClass())
+        .ordinal());
   }
 }
