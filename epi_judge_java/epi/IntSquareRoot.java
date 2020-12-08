66cbe9565341348b81c6bc9f591cbe917d4250f9
junrong09 <junrongy09@gmail.com>
2020-12-08 14:15:09 +0800

    Add completed floor square root

--- epi_judge_java/epi/IntSquareRoot.java
+++ epi_judge_java/epi/IntSquareRoot.java
@@ -6,7 +6,21 @@ public class IntSquareRoot {
 
   public static int squareRoot(int k) {
     // TODO - you fill in here.
-    return 0;
+    // O(lg n); O(1)
+    int s = 0, e = k, found = 0;
+    while (s <= e) {
+      int m = (e-s)/2 + s;
+      long mSq = (long) m * m;
+      if (mSq == k) {
+        return m;
+      } else if (mSq > k) {
+        e = m - 1;
+      } else {
+        found = Math.max(found, m);
+        s = m + 1;
+      }
+    }
+    return found;
   }
 
   public static void main(String[] args) {
