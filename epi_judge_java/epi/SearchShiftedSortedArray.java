20518a6159c576dd1f26bc3aef137089a1129561
junrong09 <junrongy09@gmail.com>
2020-12-08 16:17:56 +0800

    Add completed search shifted sorted array

--- epi_judge_java/epi/SearchShiftedSortedArray.java
+++ epi_judge_java/epi/SearchShiftedSortedArray.java
@@ -8,6 +8,20 @@ public class SearchShiftedSortedArray {
 
   public static int searchSmallest(List<Integer> A) {
     // TODO - you fill in here.
+    // O(lg n); O(1)
+    int pivot = A.get(0);
+    int s = 0, e = A.size() - 1;
+    while (s <= e) {
+      int m = s + (e-s) / 2;
+      if (m - 1 >= 0 && A.get(m-1) > A.get(m)) {
+        return m;
+      }
+      if (pivot > A.get(m)) {
+        e = m - 1;
+      } else {
+        s = m + 1;
+      }
+    }
     return 0;
   }
 
