573490209abab435d0602f28db8a995984911311
junrong09 <junrongy09@gmail.com>
2020-12-08 22:45:23 +0800

    Add completed nearest repeated entries

--- epi_judge_java/epi/NearestRepeatedEntries.java
+++ epi_judge_java/epi/NearestRepeatedEntries.java
@@ -2,13 +2,26 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
+import java.util.*;
 public class NearestRepeatedEntries {
   @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
 
   public static int findNearestRepetition(List<String> paragraph) {
     // TODO - you fill in here.
-    return 0;
+    // O(n); O(d)
+    Map<String, Integer> map = new HashMap<>();
+    int min = Integer.MAX_VALUE;
+    for (int i = 0; i < paragraph.size(); i++) {
+      String s = paragraph.get(i);
+      Integer pos = map.get(s);
+      if (pos == null) {
+        map.put(s, i);
+      } else {
+        min = Math.min(min, i - pos);
+        map.put(s, i);
+      }
+    }
+    return min == Integer.MAX_VALUE ? -1 : min;
   }
 
   public static void main(String[] args) {
