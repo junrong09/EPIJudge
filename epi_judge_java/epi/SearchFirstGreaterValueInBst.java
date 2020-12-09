10d7fd556f696eb566199f1544531369c2d282f0
junrong09 <junrongy09@gmail.com>
2020-12-09 21:40:24 +0800

    Add completed search first greater value

--- epi_judge_java/epi/SearchFirstGreaterValueInBst.java
+++ epi_judge_java/epi/SearchFirstGreaterValueInBst.java
@@ -4,13 +4,24 @@ import epi.test_framework.GenericTest;
 public class SearchFirstGreaterValueInBst {
 
   public static BstNode<Integer> findFirstGreaterThanK(BstNode<Integer> tree,
-                                                       Integer k) {
+      Integer k) {
     // TODO - you fill in here.
-    return null;
+    BstNode<Integer> ptr = tree;
+    BstNode<Integer> found = new BstNode<Integer>(Integer.MAX_VALUE);
+    while (ptr != null) {
+      if (k < ptr.data) {
+        if (ptr.data < found.data) found = ptr;
+        ptr = ptr.left;
+      } else {
+        ptr = ptr.right;
+      }
+    }
+    return found.data == Integer.MAX_VALUE ? null : found;
   }
+
   @EpiTest(testDataFile = "search_first_greater_value_in_bst.tsv")
   public static int findFirstGreaterThanKWrapper(BstNode<Integer> tree,
-                                                 Integer k) {
+      Integer k) {
     BstNode<Integer> result = findFirstGreaterThanK(tree, k);
     return result != null ? result.data : -1;
   }
@@ -18,8 +29,8 @@ public class SearchFirstGreaterValueInBst {
   public static void main(String[] args) {
     System.exit(
         GenericTest
-            .runFromAnnotations(args, "SearchFirstGreaterValueInBst.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+        .runFromAnnotations(args, "SearchFirstGreaterValueInBst.java",
+          new Object() {}.getClass().getEnclosingClass())
+        .ordinal());
   }
 }
