f7ed88657d6d06acd89ee6482a33ff5890d9e5e9
junrong09 <junrongy09@gmail.com>
2020-12-09 10:17:54 +0800

    Add is string permutable to palindrome

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -1,11 +1,29 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.util.*;
 public class IsStringPermutableToPalindrome {
   @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
   public static boolean canFormPalindrome(String s) {
     // TODO - you fill in here.
+    // O(c), O(d)
+    Map<Character, Integer> map = new HashMap<>();
+    for (char c : s.toCharArray()) {
+      int count = 0;
+      if (map.containsKey(c)) {
+        count = map.get(c);
+      }
+      count += 1;
+      map.put(c, count);
+    }
+    boolean odd = false;
+    for (int v : map.values()) {
+      if (v % 2 > 0) {
+        if (odd) return false;
+        odd = true;
+      }
+    }
     return true;
   }
 
