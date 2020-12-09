c41a8b6fd9e7d1e89a7734946ebfcb0a56121090
junrong09 <junrongy09@gmail.com>
2020-12-09 14:57:29 +0800

    Add completed intersect sorted arrays

--- epi_judge_java/epi/IntersectSortedArrays.java
+++ epi_judge_java/epi/IntersectSortedArrays.java
@@ -2,21 +2,42 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
+import java.util.*;
 public class IntersectSortedArrays {
   @EpiTest(testDataFile = "intersect_sorted_arrays.tsv")
 
   public static List<Integer> intersectTwoSortedArrays(List<Integer> A,
-                                                       List<Integer> B) {
+      List<Integer> B) {
     // TODO - you fill in here.
-    return null;
+    // O(n + m); O(1)
+    List<Integer> result = new ArrayList<>();
+    int i = 0, j = 0;
+    while (i < A.size() && j < B.size()) {
+      Integer r = null, a = A.get(i), b = B.get(j);
+      if (!result.isEmpty()) {
+        r = result.get(result.size() - 1);
+      }
+      if (a > b) {
+        j++;
+      } else if (b > a) {
+        i++;
+      } else {
+        if (r != a) {
+          result.add(a);
+        }
+        i++;
+        j++;
+      }
+    }
+    return result;
   }
+
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
-            .runFromAnnotations(args, "IntersectSortedArrays.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+        .runFromAnnotations(args, "IntersectSortedArrays.java",
+          new Object() {}.getClass().getEnclosingClass())
+        .ordinal());
   }
 }
