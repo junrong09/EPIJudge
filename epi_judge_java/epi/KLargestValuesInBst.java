ca589b4efcfcc35a2c651ce5b73730c9d594e78a
junrong09 <junrongy09@gmail.com>
2020-12-10 14:52:29 +0800

    Add completed k largest values in bst

--- epi_judge_java/epi/KLargestValuesInBst.java
+++ epi_judge_java/epi/KLargestValuesInBst.java
@@ -3,7 +3,7 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
-import java.util.Collections;
+import java.util.*;
 import java.util.List;
 import java.util.function.BiPredicate;
 public class KLargestValuesInBst {
@@ -11,8 +11,23 @@ public class KLargestValuesInBst {
 
   public static List<Integer> findKLargestInBst(BstNode<Integer> tree, int k) {
     // TODO - you fill in here.
-    return Collections.emptyList();
+    // O(h + k) time; O(h) space
+    List<Integer> result = new LinkedList<>();
+    findLarger(tree, k, result);
+    return result;
   }
+
+  public static void findLarger(BstNode<Integer> tree, int k, List<Integer> result) {
+    if (tree == null) return;
+    findLarger(tree.right, k, result);
+    if (result.size() < k) {
+      result.add(tree.data);
+    } else {
+      return;
+    }
+    findLarger(tree.left, k, result);
+  }
+
   @EpiTestComparator
   public static boolean comp(List<Integer> expected, List<Integer> result) {
     if (result == null) {
@@ -26,8 +41,8 @@ public class KLargestValuesInBst {
   public static void main(String[] args) {
     System.exit(
         GenericTest
-            .runFromAnnotations(args, "KLargestValuesInBst.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+        .runFromAnnotations(args, "KLargestValuesInBst.java",
+          new Object() {}.getClass().getEnclosingClass())
+        .ordinal());
   }
 }
