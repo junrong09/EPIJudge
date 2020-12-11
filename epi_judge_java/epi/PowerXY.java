4d6642a7b875a32fb512dc0f9dc7ce4ea0d8785c
junrong09 <junrongy09@gmail.com>
2020-12-11 14:49:06 +0800

    Add completed power xy

--- epi_judge_java/epi/PowerXY.java
+++ epi_judge_java/epi/PowerXY.java
@@ -5,14 +5,28 @@ public class PowerXY {
   @EpiTest(testDataFile = "power_x_y.tsv")
   public static double power(double x, int y) {
     // TODO - you fill in here.
-    return 0.0;
+    if (y == 1) return x;
+    if (y == 0) return 1;
+    boolean neg = false;
+    if (y < 0) {
+      neg = true;
+      y *= -1;
+    }
+    double result = power(x*x, y/2);
+    if (y % 2 == 1) {
+      result *= x;
+    }
+    if (neg) {
+      result = 1/result;
+    }
+    return result;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
-            .runFromAnnotations(args, "PowerXY.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+        .runFromAnnotations(args, "PowerXY.java",
+          new Object() {}.getClass().getEnclosingClass())
+        .ordinal());
   }
 }
