175b562faa52ecf2c98277d710659a35936760c9
junrong09 <junrongy09@gmail.com>
2020-12-11 16:09:29 +0800

    Add dutch national flag sorting

--- epi_judge_java/epi/DutchNationalFlag.java
+++ epi_judge_java/epi/DutchNationalFlag.java
@@ -4,19 +4,31 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 public class DutchNationalFlag {
   public enum Color { RED, WHITE, BLUE }
 
   public static void dutchFlagPartition(int pivotIndex, List<Color> A) {
     // TODO - you fill in here.
-    return;
+    int pivotVal = A.get(pivotIndex).ordinal();
+    int ptr1 = 0, ptr2 = A.size() - 1, ptrMove = 0;
+    while (ptrMove <= ptr2) {
+      int val = A.get(ptrMove).ordinal();
+      if (pivotVal == val) {
+        ptrMove++;
+      } else if (val < pivotVal) {
+        Collections.swap(A, ptr1++, ptrMove++);
+      } else {
+        Collections.swap(A, ptr2--, ptrMove);
+      }
+    }
   }
+
   @EpiTest(testDataFile = "dutch_national_flag.tsv")
   public static void dutchFlagPartitionWrapper(TimedExecutor executor,
-                                               List<Integer> A, int pivotIdx)
-      throws Exception {
+      List<Integer> A, int pivotIdx)
+    throws Exception {
     List<Color> colors = new ArrayList<>();
     int[] count = new int[3];
 
@@ -47,7 +59,7 @@ public class DutchNationalFlag {
 
     if (i != colors.size()) {
       throw new TestFailure("Not partitioned after " + Integer.toString(i) +
-                            "th element");
+          "th element");
     } else if (count[0] != 0 || count[1] != 0 || count[2] != 0) {
       throw new TestFailure("Some elements are missing from original array");
     }
@@ -56,8 +68,8 @@ public class DutchNationalFlag {
   public static void main(String[] args) {
     System.exit(
         GenericTest
-            .runFromAnnotations(args, "DutchNationalFlag.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+        .runFromAnnotations(args, "DutchNationalFlag.java",
+          new Object() {}.getClass().getEnclosingClass())
+        .ordinal());
   }
 }
